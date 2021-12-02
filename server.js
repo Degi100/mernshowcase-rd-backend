6052e5e720ad0f9fdfee080bd6904b980aa61bf5
degi100 <r.degering@mail.de>
2021-12-02 15:01:53 +0100

    running

--- server.js
+++ server.js
@@ -1,90 +1,89 @@
-import express from "express";
-import session from "express-session";
-import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
-import cors from "cors";
+import express from 'express';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const PORT = 3003;
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors(
+	{
+		origin: 'http://localhost:3000',
+		credentials: true
+	}
+));
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-  session({
-    resave: true,
-    saveUninitialized: true,
-    secret: process.env.SESSION_SECRET || "tempsecret",
-  })
+	session({
+		resave: true,
+		saveUninitialized: true,
+		secret: process.env.SESSION_SECRET || "tempsecret"
+	})
 );
 
 const users = [
-  {
-    username: "anonymousUser",
-    firstName: "Anonymous",
-    lastName: "User",
-    accessGroups: "loggedOutUsers",
-  },
-  {
-    username: "jj",
-    firstName: "James",
-    lastName: "JustSignedUpton",
-    accessGroups: "loggedInUsers,notApprovedUsers",
-  },
-  {
-    username: "aa",
-    firstName: "Ashley",
-    lastName: "Approvedmemberton",
-    accessGroups: "loggedInUsers, members",
-  },
-  {
-    username: "kc",
-    firstName: "Kyle",
-    lastName: "ContentEditorton",
-    accessGroups: "loggedInUsers, members, contentEditors",
-  },
-  {
-    username: "ma",
-    firstName: "Mindy",
-    lastName: "Administraton",
-    accessGroups: "loggedInUsers, members, admins",
-  },
+	{
+		username: "anonymousUser",
+		firstName: "Anonymous",
+		lastName: "User",
+		accessGroups: "loggedOutUsers"
+	},
+	{
+		username: "jj",
+		firstName: "James",
+		lastName: "JustSignedUpton",
+		accessGroups: "loggedInUsers,notApprovedUsers"
+	},
+	{
+		username: "aa",
+		firstName: "Ashley",
+		lastName: "Approvedmemberton",
+		accessGroups: "loggedInUsers, members"
+	},
+	{
+		username: "kc",
+		firstName: "Kyle",
+		lastName: "ContentEditorton",
+		accessGroups: "loggedInUsers, members, contentEditors"
+	},
+	{
+		username: "ma",
+		firstName: "Mindy",
+		lastName: "Administraton",
+		accessGroups: "loggedInUsers, members, admins"
+	}
 ];
 
 app.post("/login", (req, res) => {
-  const username = req.body.username;
-  // const password = req.body.password;
-  let user = users.find((user) => user.username === username);
-  if (user) {
-    // user = users.find(user => user.username === 'anonymousUser')
-    req.session.user = user;
-    req.session.save();
-    res.json(user);
-  } else {
-    res.status(500).send("bad access");
-  }
+	const username = req.body.username;
+	// const password = req.body.password;
+	let user = users.find(user => user.username === username);
+	if (!user) {
+		user = users.find(user => user.username === 'anonymousUser');
+	}
+	req.session.user = user;
+	req.session.save();
+	res.json(user);
 });
 
 app.get("/currentuser", (req, res) => {
-  if (req.session.user) {
-    res.json(req.session.user);
-  } else {
-    res.status(500).send("bad access");
-  }
+	let user = req.session.user;
+	if (!user) {
+		user = users.find(user => user.username === 'anonymousUser');
+	}
+	res.json(user);
 });
 
 app.get("/logout", (req, res) => {
-  req.session.destroy();
-  res.send("User logged out");
+	req.session.destroy();
+	const user = users.find(user => user.username === 'anonymousUser');
+	res.json(user);
 });
 
 app.listen(PORT, (req, res) => {
-  console.log(`API listening on port ${PORT}`);
+	console.log(`API listening on port ${PORT}`);
 });
