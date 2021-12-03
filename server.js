d653fdaa73e63da0b1e2dadfaedf065eb62c19fa
degi100 <r.degering@mail.de>
2021-12-03 16:41:23 +0100

    running

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
-import express from 'express';
-import session from 'express-session';
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import User from "./models/User.js";
 
 dotenv.config();
+
+mongoose.connect(process.env.MONGOURI);
 
 const app = express();
 const PORT = 3003;
 
-app.use(cors(
-	{
-		origin: 'http://localhost:3000',
-		credentials: true
-	}
-));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-	session({
-		resave: true,
-		saveUninitialized: true,
-		secret: process.env.SESSION_SECRET || "tempsecret"
-	})
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET || "tempsecret",
+  })
 );
 
 // const users = [
@@ -58,32 +62,37 @@ app.use(
 // 	}
 // ];
 
-app.post("/login", (req, res) => {
-	const username = req.body.username;
-	// const password = req.body.password;
-	let user = users.find(user => user.username === username);
-	if (!user) {
-		user = users.find(user => user.username === 'anonymousUser');
-	}
-	req.session.user = user;
-	req.session.save();
-	res.json(user);
+app.get("/user", async (req, res) => {
+  const user = await User.find();
+  res.json(user);
 });
 
-app.get("/currentuser", (req, res) => {
-	let user = req.session.user;
-	if (!user) {
-		user = users.find(user => user.username === 'anonymousUser');
-	}
-	res.json(user);
+app.post("/login", async (req, res) => {
+  const username = req.body.username;
+  // const password = req.body.password;
+  let user = await User.findOne({ username: username });
+  if (!user) {
+    user = await User.findOne({ username: "anonymousUser" });
+  }
+  req.session.user = user;
+  req.session.save();
+  res.json(user);
 });
 
-app.get("/logout", (req, res) => {
-	req.session.destroy();
-	const user = users.find(user => user.username === 'anonymousUser');
-	res.json(user);
+app.get("/currentuser", async (req, res) => {
+  let user = req.session.user;
+  if (!user) {
+    user = await User.findOne({ username: "anonymousUser" });
+  }
+  res.json(user);
+});
+
+app.get("/logout", async(req, res) => {
+  req.session.destroy();
+  const user = await User.findOne({ username: "anonymousUser" });
+  res.json(user);
 });
 
 app.listen(PORT, (req, res) => {
-	console.log(`API listening on port ${PORT}`);
+  console.log(`API listening on port ${PORT}`);
 });
