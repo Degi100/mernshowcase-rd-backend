4efa9e1bb0f84476b3cf4e7d1fa96715c221f9cc
degi100 <r.degering@mail.de>
2021-12-06 22:21:37 +0100

    little correction

--- models/User.js
+++ models/User.js
@@ -5,8 +5,8 @@ const UserSchema = new mongoose.Schema({
 	lastName: {'type': String, "required": true},
 	username: {'type': String, "required": true},
 	email: {'type': String, "required": true},
+	password: { 'type': String, "required": true},
 	accessGroups: {'type': String, "required": true},
-	password: { 'type': String, "required": true}
 }, 
 {
 	timestamps: true,
