7240d3983fa40126983844ca5d3ed99433c08302
degi100 <r.degering@mail.de>
2021-12-17 13:52:48 +0100

    validation Register is running

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(
     saveUninitialized: true,
     cookie: {
       httpOnly: true,
-      maxAge: 60 * 60 * 24,
+      maxAge: 60 * 60 * 1000 * 24,
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       secure: process.env.NODE_ENV === "production",
     },
